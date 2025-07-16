91101f2dd19c2cc1741571688e5ac02ac8a049ef
TsuchiyaYuki299 <onigiri774@gmail.com>
2025-07-17 05:12:10 +0900

    ステップ1～3までを表示。スタートボタンを非表示

---
+++ main.js
@@ -0,0 +1,8 @@
+// indexページでSTARTボタンを押すと、選択画面が出てくる
+function start() {
+ const select = document.getElementById("message")
+  select.innerHTML = "STEP1. 使い方レクチャー<br>STEP2. 問題<br>STEP3. 実践形式"
+  
+  const startButton = document.getElementById("start-button")
+  startButton.style.display = "none"
+}
