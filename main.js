cf73583b823b3cef2ae6ba2ea121291c6f1955fc
TsuchiyaYuki299 <onigiri774@gmail.com>
2025-07-17 05:18:30 +0900

    id名を適切なものに変更 message -> title へ

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // indexページでSTARTボタンを押すと、選択画面が出てくる
 function start() {
-  const select = document.getElementById("message")
+  const select = document.getElementById("title")
   select.innerHTML = "STEP1. 使い方レクチャー<br>STEP2. 問題<br>STEP3. 実践形式"
   
   const startButton = document.getElementById("start-button")
