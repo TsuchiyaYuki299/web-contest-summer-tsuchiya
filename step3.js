305d5e4367ee29eea4203e14d51f579abf0dcc4c
TsuchiyaYuki299 <onigiri774@gmail.com>
2025-07-26 13:51:36 +0900

    うさぎの正解イラストを追加

--- step3.js
+++ step3.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const problemElement = document.getElementById('problem-text');
 problemElement.textContent = '左のメニューからソース管理を選んで、リポジトリを初期化しよう！';
 
@@ -12,5 +14,16 @@ correctButton.classList.add('highlight-hint'); // ボタンにヒントのクラ
 document.getElementById('source-control-button').addEventListener('click', function() {
   // 点滅をとめる
   clearTimeout(hintTimer);
+  // サイドバーの内容をソース管理版に変更
+  const sideText = document.getElementById('side-text');
+  sideText.innerHTML = "ソース管理 <br> リポジトリ <br> 変更 <br> <button id='init-button'>リポジトリを初期化する</button> <br> <p id='correct-img'></p>";
   
 })
+
+
+document.getElementById('init-button').addEventListener('click', function() {
+  // p（#correct-img）をanimal_quiz_usagi_maru.pngに変更
+  const correctImg = document.getElementById('correct-img');
+  correctImg.src = 'images/animal_quiz_usagi_maru.png';
+  
+});
