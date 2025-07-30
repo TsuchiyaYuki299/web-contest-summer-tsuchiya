b93c1d3541d929ef9f5fb5a3ed606a826cb59866
TsuchiyaYuki299 <onigiri774@gmail.com>
2025-07-31 00:22:56 +0900

    step3を2問作成

--- step3.js
+++ step3.js
@@ -1,29 +1,64 @@
-'use strict';
+"use strict";
 
-const problemElement = document.getElementById('problem-text');
-problemElement.textContent = '左のメニューからソース管理を選んで、リポジトリを初期化しよう！';
+const sideText = document.getElementById("side-text");
+  
 
+const problemElement = document.getElementById("problem-text");
+problemElement.textContent =
+  "左のメニューからソース管理を選んで、リポジトリを初期化しよう！(git init)";
 
 const hintTimer = setTimeout(() => {
-// タイマーの処理の中
-const correctButton = document.getElementById('source-control-button');
-correctButton.classList.add('highlight-hint'); // ボタンにヒントのクラスを追加
-},6000);
+  // タイマーの処理の中
+  const correctButton = document.getElementById("source-control-button");
+  correctButton.classList.add("highlight-hint"); // ボタンにヒントのクラスを追加
+}, 6000);
+
+document
+  .getElementById("source-control-button")
+  .addEventListener("click", function () {
+    this.classList.remove("highlight-hint");
+    clearTimeout(hintTimer);
+    // サイドバーの内容をソース管理版に変更
+    
+    sideText.innerHTML =
+      "ソース管理 <br> リポジトリ <br> 変更 <br> <button id='init-button' onclick='init()'>リポジトリを初期化する</button> <br> <div id='correct-img'></div>";
+  });
+
+function init() {
+  if (document.getElementById("usagi-img") !== null) {
+    return;
+  }
+  // 画像を表示する場所を見つける
+  const correctImgArea = document.getElementById("correct-img");
+
+  // 新しいimg要素を作成する
+  const correctImg = document.createElement("img");
+  const correctP = document.createElement("p");
 
 
-document.getElementById('source-control-button').addEventListener('click', function() {
-  // 点滅をとめる
-  clearTimeout(hintTimer);
-  // サイドバーの内容をソース管理版に変更
-  const sideText = document.getElementById('side-text');
-  sideText.innerHTML = "ソース管理 <br> リポジトリ <br> 変更 <br> <button id='init-button'>リポジトリを初期化する</button> <br> <p id='correct-img'></p>";
+  // 作成したimg要素に、どの画像を表示するか設定する
+  correctImg.src = "images/animal_quiz_usagi_maru.png";
+
+  correctImg.alt = "正解";
+  correctImg.id = "usagi-img";
+
+correctP.innerHTML = "うさちゃんをクリックして次の問題へ！";
+
+  //  画像を表示する場所に作成したimg要素を追加する
+  correctImgArea.appendChild(correctImg);
   
-})
-
-
-document.getElementById('init-button').addEventListener('click', function() {
-  // p（#correct-img）をanimal_quiz_usagi_maru.pngに変更
-  const correctImg = document.getElementById('correct-img');
-  correctImg.src = 'images/animal_quiz_usagi_maru.png';
+correctImgArea.appendChild(correctP);
   
+correctImgArea.addEventListener("click", function () {
+problemElement.textContent = "ステージングしてからコミットしてしよう！（git add と git commit）";
+  const initButton = document.getElementById("init-button");
+  initButton.innerText = "✓コミット";
+  correctImgArea.remove();
+  sideText.append("+");
+}); 
+}
+
+sideText("+").addEventListener("click", function () {
+  sideText.append("");
+
 });
